6d47e45a9e376c471cca935f2722bd9505602d1c
Garcia Cristian <garcia.cristian.arg@gmail.com>
2025-05-15 23:44:57 -0300

    carpeta TP6

---
+++ TP6/Grafo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP6
+{
+	/// <summary>
+	/// Description of Grafo.
+	/// </summary>
+	public class Grafo<T>
+	{
+		public Grafo()
+		{
+		}
+		
+		private List<Vertice<T>>vertices = new List<Vertice<T>>();
+	
+		public void agregarVertice(Vertice<T> v) {
+			v.setPosicion(vertices.Count + 1);
+			vertices.Add(v);
+		}
+
+		public void eliminarVertice(Vertice<T> v) {
+			vertices.Remove(v);
+		}
+
+		public void conectar(Vertice<T> origen, Vertice<T> destino, int peso) {
+			origen.getAdyacentes().Add(new Arista<T>(destino,peso));
+		}
+
+		public void desConectar(Vertice<T> origen, Vertice<T> destino) {
+			Arista<T> arista = origen.getAdyacentes().Find(a => a.getDestino().Equals(destino));
+			origen.getAdyacentes().Remove(arista);
+		}
+
+	
+		public List<Vertice<T>> getVertices() {
+			return vertices;
+		}
+
+	
+		public Vertice<T> vertice(int posicion) {
+			return this.vertices[posicion];
+		}
+	
+		//---------------------------------------------------------------------------
+		public void DFS(Vertice<T> origen) 
+		{
+			bool[] visitados = new bool[this.vertices.Count];
+			this._DFS(origen, visitados);
+		}
+
+		private void _DFS(Vertice<T> origen, bool[] visitados)
+		{
+			//Proceso del origen
+			Console.WriteLine(origen.getDato() + " ");
+
+			//Cambio el flag de visitados
+			visitados[origen.getPosicion() - 1] = true;
+
+			//Llamada recursiva en profundidad
+			foreach(var adyacente in origen.getAdyacentes())
+				if (!visitados[adyacente.getDestino().getPosicion() - 1])
+					this._DFS(adyacente.getDestino(), visitados);
+		}
+
+
+        //---------------------------------------------------------------------------
+        public void BFS(Vertice<T> origen) {
+
+		}
+		
+		
+	}
+}
---
+++ TP6/MercadoDePilotos.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TP6
+{
+    public class MercadoDePilotos
+    {
+        public MercadoDePilotos() 
+        {
+            //Constructor default ya que no hay variables de instancia por enunciado
+        }
+        public List<string> PilotoQuePasoPorMasEscuderias(Grafo<string> escuderias)
+        {
+            Vertice<string> origen = escuderias.getVertices().Find(v => v.getDato() == "origen");
+
+            //Lista con la ruta mas larga (mas elementos)
+            List<string> mejorCamino = new List<string>();
+
+            foreach (Arista<string> arista in origen.getAdyacentes())
+            {
+                //Lista para analizar la ruta actua
+                List<string> caminoActual = new List<string>();
+                Vertice<string> siguiente = arista.getDestino();
+                caminoActual.Add(siguiente.getDato());
+
+                DFS(siguiente, caminoActual, mejorCamino);
+            }
+
+            return mejorCamino;
+        }
+
+        //Este método es es el que hace posible la recursión y por lo tanto el análisis de la ruta
+        //Es un método privado, ya que, los argumentos del método del enunciado son insuficientes
+        //Corresponde al algoritmo DSF
+        private void DFS(Vertice<string> actual, List<string> caminoActual, List<string> mejorCamino)
+        {
+            foreach (Arista<string> arista in actual.getAdyacentes())
+            {
+                Vertice<string> siguiente = arista.getDestino();
+
+                //Verifica que el camino actual no se encuentre en la ruta
+                //Evita usar el vector de Visitados
+                if (caminoActual.Contains(siguiente.getDato()))
+                    continue;
+
+                caminoActual.Add(siguiente.getDato());
+
+                if (caminoActual.Count > mejorCamino.Count)
+                {
+                    //Actualiza el mejor camino
+                    mejorCamino.Clear();
+                    mejorCamino.AddRange(caminoActual);
+                }
+
+                //Recursividad
+                DFS(siguiente, caminoActual, mejorCamino);
+            }
+        }
+    }
+}
+
